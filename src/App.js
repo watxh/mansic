3d70ad599a42b9ae4bc8bb8410a4e0f55d2dcef9
Jung-Min Kang <46243553+watxh@users.noreply.github.com>
2020-10-10 12:50:30 +0900

    asd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/image/test.png" width="1200px" height="auto" />
+    </div>
+  );
+}
+
+export default App;
