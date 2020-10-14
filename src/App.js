5e4404d5beb94de3bb25f489c0b31892c4332315
Jung-Min Kang <46243553+watxh@users.noreply.github.com>
2020-10-14 10:22:22 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,169 @@
-import React from "react";
-import styled from "styled-components";
+import { isAbsolute } from "path";
+import React, { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [size, setSize] = useState(10);
+
+  const [inter, setInter] = useState({ now: 0, j: 0 });
+
+  useEffect(() => {
+    let testing = function test(pos) {
+      let box = document.getElementById("box" + inter.j);
+      let top = String(pos.y - (size * 12) / 2) + "px";
+      let left = String(pos.x - (size * 18) / 2) + "px";
+      box.style.top = top;
+      box.style.left = left;
+      box.style.position = "absolute";
+      if (!inter.now) console.log("remove");
+      window.addEventListener("mouseup", () => {
+        this.removeEventListener("mousemove", testing);
+      });
+    };
+
+    if (inter.now) {
+      window.addEventListener("mousemove", testing);
+    }
+  }, [inter]);
+
+  const changeNum = (e) => {
+    setNum(e.target.value);
+  };
+
+  const changeSize = (e) => {
+    setSize(e.target.value);
+  };
+
+  const getPos = async () => {
+    const Xrows = [];
+    const Yrows = [];
+    const widthdata = document.getElementById("box" + 0).clientWidth;
+    const heightdata = document.getElementById("box" + 0).clientHeight;
+
+    for (let i = 0; i < num; i++) {
+      const box = document.getElementById("box" + i);
+
+      let Boxwidth = document.getElementById("mainimage").clientWidth;
+      let Boxheight = document.getElementById("mainimage").clientHeight;
+
+      let top = window.getComputedStyle(box).top;
+      let left = window.getComputedStyle(box).left;
+
+      let x = parseInt((parseFloat(left) / parseFloat(Boxwidth)) * 1920);
+      let y = parseInt((parseFloat(top) / parseFloat(Boxheight)) * 1080);
+
+      Xrows.push(x);
+      Yrows.push(y);
+    }
+    console.log(Xrows, Yrows, widthdata, heightdata);
+
+    const params = new URLSearchParams({
+      deskX: Xrows,
+      deskY: Yrows,
+      deskWidth: widthdata,
+      deskHeight: heightdata,
+    });
+
+    const posting = await axios.post(
+      "https://mansic-back.herokuapp.com/api/plus",
+      params
+    );
+  };
+
   return (
-    <div className="App">
-      <img src="/image/test.png" width="1200px" height="auto" />
-    </div>
+    <Container>
+      <Container1>
+        <DeskImage
+          src="/image/test.png"
+          width="100%"
+          height="auto"
+          id="mainimage"
+        />
+        <BoxContainer>
+          {(function () {
+            let rows = [];
+            for (let j = 0; j < num; j++) {
+              rows.push(
+                <Box
+                  size={size}
+                  num={num + 3}
+                  id={"box" + j}
+                  onMouseDown={() => {
+                    setInter({ now: 1, j: j });
+                  }}
+                  onMouseUp={() => {
+                    setInter({ now: 0, j: j });
+                  }}
+                />
+              );
+            }
+            return rows;
+          })()}
+        </BoxContainer>
+      </Container1>
+
+      <Container2>
+        <input type="number" value={num} onChange={changeNum} />
+        <input type="number" value={size} onChange={changeSize} />
+        <br />
+        <button onClick={getPos}>완료</button>
+      </Container2>
+    </Container>
   );
-}
+};
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Container1 = styled.div`
+  width: 70%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+`;
+
+const Container2 = styled.div`
+  width: 30%;
+`;
+
+const DeskImage = styled.img`
+  position: absolute;
+  z-index: 2;
+  pointer-events: none;
+`;
+
+const BoxContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Box = styled.div`
+  width: 180px;
+  height: 120px;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 3;
+  position: relative;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+
+  ${({ num }) =>
+    num &&
+    css`
+      z-index: ${num};
+    `};
+
+  ${({ size }) =>
+    size &&
+    css`
+      width: ${size * 18}px;
+      height: ${size * 12}px;
+    `};
+`;
 
 export default App;
